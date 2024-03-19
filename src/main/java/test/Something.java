332d22eff69cb838ff2a8b7189a60a8cdc3b78cc
Matyrobbrt <matyrobbrt@gmail.com>
2024-03-19 22:26:57 +0200

    Hmmm

--- src/main/java/test/Something.java
+++ src/main/java/test/Something.java
@@ -1,15 +1,13 @@
 package test;
 
-public class Something {
+public abstract class Something {
     public static void main(String[] args) {
         System.out.println("A");
     }
 
     public final String field1 = "a";
 
-    void hi() {
-
-    }
+    abstract void hi();
 
     public static class C1 {
         public void run() {
